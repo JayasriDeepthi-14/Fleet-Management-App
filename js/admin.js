e137ab704bedbd9fc3b3b4ea5e57fa94193cc12d
Sabbella Jayasri Deepthi <arunkumarreddymedapati@gmail.com>
2026-02-20 21:59:46 +0530

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -52,7 +52,7 @@ function renderCards(data) {
         card.className = "card";
 
         card.innerHTML = `
-            <img src="${imageURL}" style="height:150px; width:300px;"/>
+            <img src="${imageURL}" style="height:auto; width:100%;"/>
 
             <h4>${vehicle.regNo}</h4>
             <p>Category: ${vehicle.category}</p>
